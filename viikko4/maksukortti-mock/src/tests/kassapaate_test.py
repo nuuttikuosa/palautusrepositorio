2b8f39240a59237057debce799337bc73c6dd578
Nuutti Kuosa <nuutti.kuosa@helsinki.fi>
2023-11-23 23:12:59 +0200

    viikko2 tehtävä 2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_ladataan_rahaa_jos_ladattava_maara_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        
+        self.kassa.lataa(maksukortti_mock, 5)
+
+        maksukortti_mock.lataa.assert_called_with(5)
+
+    def test_kortille_ei_ladata_rahaa_jos_ladattava_maara_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        
+        self.kassa.lataa(maksukortti_mock, -5)
+
+        maksukortti_mock.lataa.assert_not_called()
