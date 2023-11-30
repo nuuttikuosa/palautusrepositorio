a62c39a0670a0c414c25a63fc734aa8c45eb8bf5
Nuutti Kuosa <nuutti.kuosa@helsinki.fi>
2023-11-30 23:58:05 +0200

    viikko 5 tehtävä 4

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,15 +1,53 @@
+class Summa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self.sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+        
+
+
+    
+    def suorita(self):
+        try:
+            operandi =  int(self._lue_syote())
+        except Exception:
+            pass
+        self.sovelluslogiikka.aseta_arvo(self.sovelluslogiikka.arvo() + operandi)
+
+class Erotus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self.sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+    
+    def suorita(self):
+
+        try:
+            operandi =  int(self._lue_syote())
+        except Exception:
+            pass
+        self.sovelluslogiikka.aseta_arvo(self.sovelluslogiikka.arvo() - operandi)
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self.sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+    
+    
+    def suorita(self):
+        self.sovelluslogiikka.aseta_arvo(0)
+
+
 class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
 
-    def miinus(self, operandi):
-        self._arvo = self._arvo - operandi
+   # def miinus(self, operandi):
+    #    self._arvo = self._arvo - operandi
 
-    def plus(self, operandi):
-        self._arvo = self._arvo + operandi
+    #def plus(self, operandi):
+    #    self._arvo = self._arvo + operandi
 
-    def nollaa(self):
-        self._arvo = 0
+    #def nollaa(self):
+    #    self._arvo = 0
 
     def aseta_arvo(self, arvo):
         self._arvo = arvo
