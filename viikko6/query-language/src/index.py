29372627f223c52bc4e98cf3ce14efca1002c0b2
Nuutti Kuosa <nuutti.kuosa@helsinki.fi>
2023-12-09 02:02:32 +0200

    tehtävä 2

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -25,12 +25,31 @@ def main():
     PlaysIn("NYR")
 )
 
-    print("")
+    print("Tehtävä 1")
     for player in stats.matches(matcher):
         print(player)
 
     filtered_with_all = stats.matches(All())
     print(len(filtered_with_all))
+
+    print("Tehtävä 2")
+    matcher = Or(
+    HasAtLeast(45, "goals"),
+    HasAtLeast(70, "assists")
+)
+    for player in stats.matches(matcher):
+        print(player)
+
+    matcher = And(
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+    )
+)
+    for player in stats.matches(matcher):
+        print(player)
 
 if __name__ == "__main__":
     main()
